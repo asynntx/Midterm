57d01ac1bfef88fccd4556346e9301d0d41e70c8
unknown <YY241@ccs.local>
2025-03-10 05:57:40 -0700

    question3

---
+++ question3.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! Ashley Kyle Tanglao'});    
+});
+
+app.listen(PORT, () => {
+    console.log (`Server is running running on http`);
+});
